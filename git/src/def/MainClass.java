2eced605390ff6552e70f374e32fab4586173c9d
Zabo <Zabo@Zabo-PC>
2012-12-11 22:10:11 +0100

    java

---
+++ git/src/def/MainClass.java
@@ -0,0 +1,22 @@
+package def;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0; i<5; i++){
+			
+		}			System.out.println("asda");
+
+///sasadjsadhgasd
+		
+		Scanner scan = new Scanner(System.in);
+	}
+
+}
